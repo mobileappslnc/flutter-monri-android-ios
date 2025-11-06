f0bfca60b9d6e1d71b860d155fcefbae966a5381
mobileappslnc <mobileapps@lanaco.com>
2025-11-07 00:11:16 +0100

    Update MonriPaymentsPlugin.java

--- android/src/main/java/com/monri/flutter/MonriPaymentsPlugin.java
+++ android/src/main/java/com/monri/flutter/MonriPaymentsPlugin.java
@@ -41,8 +41,8 @@ public class MonriPaymentsPlugin implements FlutterPlugin, MethodCallHandler, Ac
     private Monri monri;
 
     private void initMonri() {
-        if (activity != null && monri == null) {
-            monri = new Monri((ActivityResultCaller)this.activity);
+        if (activityBinding != null && monri == null) {
+            monri = new Monri(activityBinding);
         }
     }
 
