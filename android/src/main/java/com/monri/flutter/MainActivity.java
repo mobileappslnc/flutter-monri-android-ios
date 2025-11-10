212a48e80468e695e7b0a639266edf8b52bd15f4
Dejan Cancar <dejan.cancar97@gmail.com>
2025-11-10 09:57:17 +0100

    Changed Monri constructor params

--- android/src/main/java/com/monri/flutter/MainActivity.java
+++ android/src/main/java/com/monri/flutter/MainActivity.java
@@ -1,10 +1,10 @@
 package com.monri.flutter;
 
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
